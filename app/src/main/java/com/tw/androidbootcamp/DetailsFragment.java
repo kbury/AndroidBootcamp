8b8c7ab86c0f0359ae775924f7b7c5f579c79151
Krystyna Bury <kbury@thoughtworks.com>
2014-09-18 19:15:38 +1000

    kbury: Display photo taken with camera.

--- app/src/main/java/com/tw/androidbootcamp/DetailsFragment.java
+++ app/src/main/java/com/tw/androidbootcamp/DetailsFragment.java
@@ -1,10 +1,18 @@
 package com.tw.androidbootcamp;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,11 +22,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class DetailsFragment extends Fragment {
     private static final String ARG_RESTAURANT_ID = "restaurantId";
+    private static final int CAMERA_ACTIVITY = 1;
 
     private long restaurantId;
+    private Uri fileUri;
+    private ImageView ivPhoto;
 
     public static DetailsFragment newInstance(long restaurantId) {
         DetailsFragment fragment = new DetailsFragment();
@@ -46,6 +61,19 @@ public class DetailsFragment extends Fragment {
         TextView tvRestId = (TextView) view.findViewById(R.id.tv_details_rest_id);
         tvRestId.setText(String.format("You are viewing details for restaurant %s", restaurantId));
 
+        ivPhoto = (ImageView) view.findViewById(R.id.picture);
+        ivPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                fileUri = Uri.fromFile(getOutputMediaFile());
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+                startActivityForResult(intent, CAMERA_ACTIVITY);
+            }
+        });
+
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         GoogleMap map = mapFragment.getMap();
 
@@ -58,6 +86,36 @@ public class DetailsFragment extends Fragment {
         return view;
     }
 
+    private static File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "AndroidBootcamp");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                Log.d("AndroidBootcamp", "failed to create directory");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyy_MM_dd").format(new Date());
+
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case CAMERA_ACTIVITY:
+                if (ivPhoto != null) {
+                    Bitmap bmp = BitmapFactory.decodeFile(fileUri.getPath());
+                    ivPhoto.setImageBitmap(bmp);
+                }
+        }
+    }
 
     public void updateView(long restaurantId) {
         TextView tvRestId = (TextView) getView().findViewById(R.id.tv_details_rest_id);
